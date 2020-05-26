7ee983b4326c0a9b6b281d434cfaeebf93a51a08
Debanshu Datta <debanshudatta123@gmail.com>
2020-05-26 16:09:18 +0530

    Automatic Slider & Slider indicator

--- app/src/main/java/com/example/moviestreamingapp/MainActivity.java
+++ app/src/main/java/com/example/moviestreamingapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.moviestreamingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
+    private List<Slide> listSlides;
+    private ViewPager sliderpager;
+    private TabLayout indicator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sliderpager=findViewById(R.id.slider_pager);
+        indicator=findViewById(R.id.indicator);
+
+        listSlides=new ArrayList<>();
+        listSlides.add(new Slide(R.drawable.slider1,"Slider Title \nsome text here"));
+        listSlides.add(new Slide(R.drawable.slider2,"Slider Title \nsome text here"));
+        listSlides.add(new Slide(R.drawable.slider1,"Slider Title \nsome text here"));
+        listSlides.add(new Slide(R.drawable.slider2,"Slider Title \nsome text here"));
+
+        SlidePagerAdapter adapter=new SlidePagerAdapter(this,listSlides);
+        sliderpager.setAdapter(adapter );
+        Timer timer=new Timer();
+        timer.scheduleAtFixedRate(new SliderTimer(),3000,3000);
+        indicator.setupWithViewPager(sliderpager,true);
+    }
+
+    class SliderTimer extends TimerTask{
+
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(sliderpager.getCurrentItem()<listSlides.size()-1){
+                        sliderpager.setCurrentItem(sliderpager.getCurrentItem()+1);
+                    }
+                    else
+                        sliderpager.setCurrentItem(0);
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/example/moviestreamingapp/SlidePagerAdapter.java
@@ -0,0 +1,53 @@
+package com.example.moviestreamingapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class SlidePagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+    private List<Slide> mList;
+
+    public SlidePagerAdapter(Context mContext, List<Slide> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size() ;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater=(LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View sliderLayout= inflater.inflate(R.layout.slide_item,null);
+
+        ImageView slideImage =sliderLayout.findViewById(R.id.slide_image);
+        TextView sliderText = sliderLayout.findViewById(R.id.slide_title);
+        slideImage.setImageResource(mList.get(position).getImage());
+        sliderText.setText(mList.get(position).getTitle());
+        container.addView(sliderLayout);
+        return  sliderLayout;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+}
